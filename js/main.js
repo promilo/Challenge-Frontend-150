ad40e49050a5bf66300ecdafe5fe82673ee65916
promilo <personalmilind@gmail.com>
2017-06-26 21:23:09 +0000

    Finished making generating users

--- js/main.js
+++ js/main.js
@@ -15,17 +15,56 @@ var options = {
 
 var generateUrl = new ApiGetClient(options);
 
-$(document).ready(function() {
-  
-  var urlCall = generateUrl.getRequestUrl()
+var urlCall = generateUrl.getRequestUrl()
+
+function renderMember(user) {
+  var $user = $('<div>').addClass('user');
+  $user.append($('<img>'). attr("src", user.picture.large))
+  var name = user.name.first + user.name.last;
+  $user.append($('<h4>').text(name));
+  $user.append($('<p>').text(user.email))
+  return $user;
+}
+
+function generateRowUsers (users) {
+  var $rowUsers = $('<div>').addClass('rowUsers')
+  for (var user of users.results) {
+      $($rowUsers).append(renderMember(user))
+    }
+  console.log($rowUsers)
+  return $rowUsers;
+};
+
+
+function generateRandom () {
   $.ajax({
   url: urlCall,
   dataType: 'json',
   success: function(data) {
-    console.log(data);
+    var $data = generateRowUsers(data);
+    $('.members').append($data);
   }
-});
+  });
+}
 
 
+
+
+$(document).ready(function() {
+  var counter = 1
+  generateRandom()
+  $("#js-btn-action-more").on('click', function (e) {
+    e.preventDefault();
+    console.log("counter: ", counter)
+    if (counter >= 5) {
+      $("#js-btn-action-more").css('background-color', 'red');
+      $("#js-btn-action-more").text('Maxed out');
+    }
+    else {
+      counter++;
+      generateRandom()
+    }
+  }
+)
 
 });
