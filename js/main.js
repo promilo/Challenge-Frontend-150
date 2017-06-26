d94f66dae24d307d86852344a6f87d7c3d7427bd
promilo <personalmilind@gmail.com>
2017-06-26 20:15:16 +0000

    Added ajax call

--- js/main.js
+++ js/main.js
@@ -1,1 +1,31 @@
 // Your code here ...
+"use strict";
+
+var options = {
+        headers: {
+          Accept: 'application/json'
+        },
+        method: 'get',
+        queryparams: {
+          results: 5
+        },
+        apibaseurl: 'https://randomuser.me',
+        apipath: '/api'
+}
+
+var generateUrl = new ApiGetClient(options);
+
+$(document).ready(function() {
+  
+  var urlCall = generateUrl.getRequestUrl()
+  $.ajax({
+  url: urlCall,
+  dataType: 'json',
+  success: function(data) {
+    console.log(data);
+  }
+});
+
+
+
+});
